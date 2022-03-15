dc29f9be71aae408af87ccf2e5c3a84850a4a94c
DuliniDilmini <ddpwithanage@gmail.com>
2022-03-15 18:59:27 +0530

    Practice ng switch core directive and built in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ import { Course } from './model/course';
 export class AppComponent {
   courses = COURSES;
 
+  title = COURSES[0].description;
+
+  price = 9.99;
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
+  startDate = new Date(2000, 0, 1);
+
   coreCourse = COURSES[0];
 
   rxjsCourse = COURSES[1];
